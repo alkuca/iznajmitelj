086ce1da937f227a06f2eaa14fe739a2f51a3002
Alen <alkuca@gmail.com>
2021-07-17 14:09:18 +0200

    Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Navbar from './components/Navbar'
 import Sidebar from "./components/Sidebar";
+import DashboardContent from "./components/DashboardContent";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-        <Sidebar/>
+      <Sidebar/>
+      <DashboardContent/>
     </div>
   );
 }
---
+++ client/src/components/DashboardContent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+class DashboardContent extends React.Component {
+    render(){
+        return (
+            <div className="dashboard-container">
+                <div className="dashboard-content">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default DashboardContent;
