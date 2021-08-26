4074ca28c9bc9898445873f543fd485e88042f41
Alen <alkuca@gmail.com>
2021-08-26 02:19:02 +0200

    cors moved

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ app.use(express.static('public'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+const handleCORS = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(handleCORS);
+app.use(express.methodOverride());
+
 //middleware
 
 app.use(express.json()); // req.body
