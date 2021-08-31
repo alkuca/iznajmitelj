94719bc9b4671e5893298a5ffd9563136d7d3828
Alen <alkuca@gmail.com>
2021-08-31 13:43:43 +0200

    readme

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# iznajmitelj
 
-![alt text](https://github.com/alkuca/iznajmitelj/blob/main/client/src/images/LogoF.svg?raw=true)
+
+<br />
+<p align="center">
+  <a href="https://github.com/othneildrew/Best-README-Template">
+    <img src="https://github.com/alkuca/iznajmitelj/blob/main/client/src/images/LogoF.svg?raw=true" alt="Logo" width="80" height="80">
+  </a>
+  <h3 align="center">Rentmi</h3>
+  <p align="center">
+    Web aplikacija za iznajmljivane i unajmljivane dobara
+    <br />
+    <br />
+    <a href="https://rentmi.herokuapp.com/">Live App</a>
+  </p>
+</p>
+<br />
+<img src="https://res.cloudinary.com/dkwlvbeyk/image/upload/v1630408521/misc/moje_stvari_zszcvm.png" alt="Dashboard">
+<br />
+<br />
+
+### Korištenje tehnologije
+PERN Stack
+
+### Svrha aplikacije
+<p>
+Aplikacija pruža korisniku pregled proizvoda koji su trenutno dostupni za iznajmljivanje.
+Također korisnik ima opciju dodavanja i objavljivanja svoj vlastitih proizvoda.
+</p>
--- server/database.sql
+++ server/database.sql
@@ -1,12 +1,86 @@
-CREATE DATABASE najmi;
-
-create extension if not exists "uuid-ossp";
-
-CREATE TABLE users
+CREATE TABLE IF NOT EXISTS public.items
 (
-    user_id       uuid PRIMARY KEY DEFAULT
-    uuid_generate_v4(),
-    user_name     VARCHAR(255) NOT NULL,
-    user_email    VARCHAR(255) NOT NULL,
-    user_password VARCHAR(255) NOT NULL
-);
+    item_id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    item_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    item_description character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    item_price integer NOT NULL,
+    item_image character varying(255) COLLATE pg_catalog."default",
+    item_state character varying(255) COLLATE pg_catalog."default",
+    item_owner uuid NOT NULL,
+    item_posted boolean DEFAULT false,
+    item_street character varying(255) COLLATE pg_catalog."default",
+    item_street_number character varying(255) COLLATE pg_catalog."default",
+    item_city character varying(255) COLLATE pg_catalog."default",
+    item_lat character varying(255) COLLATE pg_catalog."default",
+    item_long character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT item_pkey PRIMARY KEY (item_id)
+)
+
+CREATE TABLE IF NOT EXISTS public.messages
+(
+    message_id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    sender_id uuid NOT NULL,
+    receiver_id uuid NOT NULL,
+    message_title character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    message_text character varying(255) COLLATE pg_catalog."default",
+    sender_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    message_is_read boolean DEFAULT false,
+    receiver_name character varying(255) COLLATE pg_catalog."default",
+    time_created timestamp(6) without time zone DEFAULT (now())::timestamp without time zone,
+    CONSTRAINT messages_pkey PRIMARY KEY (message_id)
+)
+
+CREATE TABLE IF NOT EXISTS public.notifications
+(
+    notification_id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    notification_owner_id uuid NOT NULL,
+    notification_maker_id uuid NOT NULL,
+    notification_maker_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    notification_type character varying(255) COLLATE pg_catalog."default",
+    related_item_id uuid,
+    related_item_name character varying(255) COLLATE pg_catalog."default",
+    delivery_type boolean,
+    time_created character varying(255) COLLATE pg_catalog."default",
+    clear_notification boolean DEFAULT false,
+    return_type integer,
+    CONSTRAINT notifications_pkey PRIMARY KEY (notification_id)
+)
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+    user_id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    user_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    user_email character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    user_password character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    user_street character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    user_city character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    user_state character varying COLLATE pg_catalog."default" NOT NULL,
+    lat character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    "long" character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    user_street_number character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    user_image character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT users_pkey PRIMARY KEY (user_id)
+    )
+
+CREATE TABLE IF NOT EXISTS public.rented_items
+(
+    rented_item_id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    owner_id uuid NOT NULL,
+    renter_id uuid NOT NULL,
+    item_id uuid NOT NULL,
+    duration integer NOT NULL,
+    price_per_day integer NOT NULL,
+    price integer NOT NULL,
+    item_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    delivery_type integer NOT NULL,
+    paid boolean,
+    code_entered boolean DEFAULT false,
+    renting_status boolean DEFAULT true,
+    confirmation_code character varying(255) COLLATE pg_catalog."default" NOT NULL DEFAULT floor(((10000)::double precision + (random() * (89999)::double precision))),
+    renter_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    owner_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    return_type integer,
+    time_rent_started timestamp(6) without time zone DEFAULT (now())::timestamp without time zone,
+    item_image character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT "rentedItems_pkey" PRIMARY KEY (rented_item_id)
+)
